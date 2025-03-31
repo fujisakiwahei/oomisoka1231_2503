2943ffa83295ee7d1657504eb90b0c52d513984d
Wahei's Mac mini <fujisakiwahei@gmail.com>
2025-03-31 13:15:22 +0900

    サンクスページのNuxtImgをimgに変更

--- pages/thanks.vue
+++ pages/thanks.vue
@@ -7,7 +7,7 @@
         お問い合わせ<br class="md:hidden" />ありがとうございました！
       </p>
       <p class="mx-auto w-full md:w-96">
-        <NuxtImg
+        <img
           src="@/assets/images/thanks/thanks_illust.webp"
           width="384"
           height="600"
